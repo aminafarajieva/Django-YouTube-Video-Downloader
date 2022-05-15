87e1ce334077021c205c30607b019c0f0b1f4da8
Amina <ameena_farage.yahoo.com>
2022-05-15 15:48:53 +0400

    First commit

---
+++ YTdlApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from pytube import YouTube
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        video = YouTube(link)
+        stream = video.streams.first()
+        stream.download()
+        return render(request,'index.html',{'msg':'Video downloaded'})
+    return render(request,'index.html',{'msg':'Video not downloaded'})
